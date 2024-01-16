5961d210db4f5af44cd55711abd6f9eaef39feee
juan Manuel Gaitan <jmgaitan93@gmail.com>
2024-01-16 17:40:58 -0300

    diseño final del header y correcciones del footer

---
+++ js/footer.js
@@ -0,0 +1,34 @@
+function addFooter() {
+
+    const footer = document.createElement('footer');
+    footer.className = 'footer';
+
+    const footerSocial = document.createElement('div');
+    footerSocial.className = 'footer-social';
+    const socialLink = document.createElement('a');
+    socialLink.href = 'https://github.com/jmgaitan';
+    const socialImage = document.createElement('img');
+    socialImage.src = './images/icons/github-mark-white.svg';
+    socialImage.alt = 'Perfil de GitHub';
+    socialLink.appendChild(socialImage);
+    footerSocial.appendChild(socialLink);
+    footer.appendChild(footerSocial);
+
+    const linkContainer = document.createElement('span');
+    linkContainer.className = 'link-container';
+    const nosotrosLink = document.createElement('a');
+    nosotrosLink.href = '/nosotros.html';
+    nosotrosLink.className = 'footer-link';
+    nosotrosLink.textContent = 'Nosotros';
+    const contactoLink = document.createElement('a');
+    contactoLink.href = '/contacto.html';
+    contactoLink.className = 'footer-link';
+    contactoLink.textContent = 'Contacto';
+
+    linkContainer.appendChild(nosotrosLink);
+    linkContainer.appendChild(contactoLink);
+    footer.appendChild(linkContainer);
+
+    document.body.appendChild(footer);
+}
+
